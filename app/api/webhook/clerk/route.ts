eb67600e7606bd24da074a5cb44c3ef3933e18c3
SuDy0906 <127987188+SuDy0906@users.noreply.github.com>
2025-02-10 16:01:40 +0530

    Update route.ts

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -55,7 +55,7 @@ export async function POST(req: Request) {
   const eventType = evt.type;
  
   if(eventType === 'user.created') {
-    const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
+    const { id, email_addresses, image_url, first_name, last_name, userName } = evt.data;
 
     const user = {
       clerkId: id,
@@ -80,12 +80,12 @@ export async function POST(req: Request) {
   }
 
   if (eventType === 'user.updated') {
-    const {id, image_url, first_name, last_name, username } = evt.data
+    const {id, image_url, first_name, last_name, userName } = evt.data
 
     const user = {
       firstName: first_name,
       lastName: last_name,
-      username: username!,
+      username: userName!,
       photo: image_url,
     }
 
